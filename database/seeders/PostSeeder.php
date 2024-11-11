1ffd92ee1014061a7924fc1fbc671333f2783e4d
natdm02 <natalidimarino2002@gmail.com>
2024-11-11 12:40:05 +0100

    [add] seeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Post;
 
 class PostSeeder extends Seeder
 {
@@ -12,6 +13,23 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Post::create([
+
+            'title' => '1 title',
+            'content' => '1 content',
+            'author' => '1 author',
+        ]);
+
+        Post::create([
+            'title' => '2 title',
+            'content' => '2 content',
+            'author' => '2 author',
+        ]);
+
+        Post::create([
+            'title' => '3 title',
+            'content' => '3 content',
+            'author' => '3 author',
+        ]);
     }
 }
