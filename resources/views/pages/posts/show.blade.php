00bae9575ad99a674b436419108ad384ee49c191
orazi-paolo <orazi.paolo10@gmail.com>
2024-11-11 12:34:24 +0100

    hacking

--- resources/views/pages/posts/show.blade.php
+++ resources/views/pages/posts/show.blade.php
@@ -1,10 +1,15 @@
 @extends("layouts.app")
-
+@section("page-title", show);
 @section("page-title", "Show")
 
 @section("main-content")
 <main class="container">
     <div class="card" style="width: 18rem;">
+        ciao mi chiamo paolo e faccio gli scherzetti a kacopo
+
+
+
+        ti posso regalare un lanciafiamme?
 
     </div>
 </main>
