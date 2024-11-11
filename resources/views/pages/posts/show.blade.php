1c6d372b0be4fea276bca472128df3450fad8598
jacopolegovini <legovini96@gmail.com>
2024-11-11 12:35:01 +0100

    view show completed

--- resources/views/pages/posts/show.blade.php
+++ resources/views/pages/posts/show.blade.php
@@ -5,7 +5,20 @@
 @section("main-content")
 <main class="container">
     <div class="card" style="width: 18rem;">
-
+        <div class="card-body">
+            <h5 class="card-title">{{$posts->title}}</h5>
+            <ul>
+                <li>
+                    <p>Autore: {{$posts->author}}</p>
+                </li>
+                <li>
+                    <p>Descrizione: {{$posts->description}}</p>
+                </li>
+                <li>
+                    <p>Data: {{$posts->date}}</p>
+                </li>
+            </ul>
+        </div>
     </div>
 </main>
 @endsection
